95c90e4a46402b20b0b2bcee7a3653b085ac64b1
Sebastian Cascante <sebastian.cascante.bd@gmail.com>
2026-07-02 22:21:34 -0600

    Fixed typo on dom.js file

--- semanas/semana_7/dom.js
+++ semanas/semana_7/dom.js
@@ -3,34 +3,34 @@ window.onload = function(){
     console.log(button1);
 }
 
-let contador = 0;
+let counter = 0;
 
 const button2 = document.getElementById("button2");
 
 
 //first way anon function
 button2.onclick = function(){
-    contador++;
-    console.log(contador);
+    counter++;
+    console.log(counter);
     
 }
 
 function function1 () {
-    console.log("funcion 1");
+    console.log("function 1");
 }
 
 
 //second way onclick
 function function2 () {
-    console.log("funcion 2");
+    console.log("function 2");
 }
 
 
 //third way listener
 const button3 = document.getElementById("button3");
 
-button3.addEventListener("click", funcion3);
+button3.addEventListener("click", function3);
 
 function function3 () {
-    console.log("funcion 3");
+    console.log("function 3");
 }
